c68d813f5efd5375d5ab1bb5ecf63b3afe16f9c2
Andrew Hearin <andrew.hearin@yale.edu>
2017-10-06 12:07:49 -0500

    Finished preliminary implementation of fake_source_galaxy_catalog

--- galsampler/tests/fake_catalogs.py
+++ galsampler/tests/fake_catalogs.py
@@ -4,13 +4,15 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 from scipy.special import erf
+from scipy.stats import poisson
 
 from ..numpy_random_context import NumpyRNGContext
 
 __all__ = ('fake_source_galaxy_catalog', )
 
 
-source_gals_type_list = list((('gal_id', 'i8'), ('mass', 'f4'), ('conc', 'f4'),
+source_gals_type_list = list((('gal_id', 'i8'),
+    ('mass', 'f4'), ('conc', 'f4'), ('host_rvir', 'f4'),
     ('satellite', '?'), ('host_halo_id', 'i8'),
     ('x', 'f4'), ('y', 'f4'), ('z', 'f4'),
     ('host_x', 'f4'), ('host_y', 'f4'), ('host_z', 'f4')))
@@ -23,15 +25,70 @@ default_dt_target_halos = np.dtype([(str(a[0]), str(a[1])) for a in target_halos
 
 
 def fake_source_galaxy_catalog(num_source_gals=int(1e3),
-        dt_source_gals=default_dt_source_gals, seed=None, Lbox=250.):
+        dt_source_gals=default_dt_source_gals, seed=None, Lbox=250.,
+        logM_min=12, sigma_logM=0.25):
     """
+    Examples
+    --------
+    >>> galaxy_catalog = fake_source_galaxy_catalog()
     """
     num_source_halos = num_source_gals*5
-    halo_mass_array = mc_halo_mass(num_source_halos, seed=seed)
+
+    m = 10**mc_halo_mass(num_source_halos, seed=seed)
+    host_x = np.random.uniform(1, Lbox-1, num_source_halos)
+    host_y = np.random.uniform(1, Lbox-1, num_source_halos)
+    host_z = np.random.uniform(1, Lbox-1, num_source_halos)
+    host_conc = 10**np.random.normal(
+        loc=np.log10(mean_halo_concentration(np.log10(m))), scale=0.1)
+    rvir = np.random.rand(num_source_halos)
+
+    mc_ncen = np.random.rand(num_source_halos) < mean_ncen(np.log10(m), logM_min, sigma_logM)
+    mc_nsat = poisson.rvs(mean_nsat(m, logM_min))
+    ngal = mc_ncen + mc_nsat
+    halo_mass_array = np.repeat(m, ngal)
+    halo_id_galaxies = np.repeat(np.arange(len(m)).astype(int), ngal)
+    gal_id_array_galaxies = np.arange(len(halo_id_galaxies)).astype(int)
+    host_x_galaxies = np.repeat(host_x, ngal)
+    host_y_galaxies = np.repeat(host_y, ngal)
+    host_z_galaxies = np.repeat(host_z, ngal)
+    host_rvir_galaxies = np.repeat(rvir, ngal)
+    host_conc_galaxies = np.repeat(host_conc, ngal)
+    unique_ids, idx = np.unique(halo_id_galaxies, return_index=True)
+    satellite = np.ones(len(halo_id_galaxies), dtype=bool)
+    satellite[idx] = False
 
     galaxy_catalog = np.zeros(num_source_gals, dtype=dt_source_gals)
+    galaxy_catalog['gal_id'] = gal_id_array_galaxies[:num_source_gals]
+    galaxy_catalog['mass'] = halo_mass_array[:num_source_gals]
+    galaxy_catalog['conc'] = host_conc_galaxies[:num_source_gals]
+    galaxy_catalog['satellite'] = satellite[:num_source_gals]
+    galaxy_catalog['host_halo_id'] = halo_id_galaxies[:num_source_gals]
+    galaxy_catalog['host_x'] = host_x_galaxies[:num_source_gals]
+    galaxy_catalog['host_y'] = host_y_galaxies[:num_source_gals]
+    galaxy_catalog['host_z'] = host_z_galaxies[:num_source_gals]
+    galaxy_catalog['x'] = host_x_galaxies[:num_source_gals]
+    galaxy_catalog['y'] = host_y_galaxies[:num_source_gals]
+    galaxy_catalog['z'] = host_z_galaxies[:num_source_gals]
+    galaxy_catalog['host_rvir'] = host_rvir_galaxies[:num_source_gals]
+
+    satmask = galaxy_catalog['satellite'] == True
+    nsats = np.count_nonzero(satmask)
+    with NumpyRNGContext(seed):
+        dx = np.random.uniform(0, 1/3., nsats)*galaxy_catalog['host_rvir'][satmask]
+        dy = np.random.uniform(0, 1/3., nsats)*galaxy_catalog['host_rvir'][satmask]
+        dz = np.random.uniform(0, 1/3., nsats)*galaxy_catalog['host_rvir'][satmask]
+
+    galaxy_catalog['x'][satmask] += dx
+    galaxy_catalog['y'][satmask] += dy
+    galaxy_catalog['z'][satmask] += dz
 
     return galaxy_catalog
+
+
+def mean_halo_concentration(logM):
+    logM_table = [10, 15]
+    conc_table = [15, 4.1]
+    return np.interp(logM, logM_table, conc_table)
 
 
 def mean_ncen(logM, logM_min, sigma_logM):
@@ -40,7 +97,7 @@ def mean_ncen(logM, logM_min, sigma_logM):
 
 def mean_nsat(m, logM_min):
     log_M0 = logM_min-0.3
-    log_M1 = log_M0+1.5
+    log_M1 = log_M0+2
     M0, M1 = 10**log_M0, 10**log_M1
     return np.maximum((m - M0)/M1, 0.0001)
 
