1391f484bf0bd51a06104367e43a76c3427b81cb
Andrew Hearin <andrew.hearin@yale.edu>
2017-10-18 13:38:04 -0500

    Cythonized source_galaxy_index_selection function

--- galsampler/cython_kernels/galaxy_selection_kernel.pyx
+++ galsampler/cython_kernels/galaxy_selection_kernel.pyx
@@ -12,8 +12,25 @@ __all__ = ('source_galaxy_index_selection_kernel', )
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
-def source_galaxy_index_selection_kernel(long[:] first_index_source_bin,
-            long[:] last_index_source_bin, long[:] num_select_per_bin):
+def source_galaxy_index_selection_kernel(long[:] idx, long[:] n):
     """
     """
-    return None
+    cdef int num_source_halos = idx.shape[0]
+
+    cdef int i, num_target_gals
+    for i in range(num_source_halos):
+        num_target_gals += n[i]
+
+    cdef long[:] indices = np.zeros(num_target_gals).astype(long)
+
+    cdef long cur = 0
+    cdef long ifirst, j, nselect
+
+    for i in range(num_source_halos):
+        ifirst = idx[i]
+        nselect = n[i]
+        for j in range(nselect):
+            indices[cur] = ifirst + j
+            cur += 1
+
+    return indices
--- galsampler/source_galaxy_selection.py
+++ galsampler/source_galaxy_selection.py
@@ -12,17 +12,7 @@ def source_galaxy_index_selection(idx, n):
     >>> n = np.random.randint(1, 5, 100)
     >>> indices = source_galaxy_index_selection(idx, n)
     """
-    num_source_halos = len(idx)
-    num_target_gals = np.sum(n)
-    indices = np.zeros(num_target_gals).astype(long)
-
-    cur = 0
-    for i in range(num_source_halos):
-        ifirst = idx[i]
-        nselect = n[i]
-        for j in range(nselect):
-            indices[cur] = ifirst + j
-            cur += 1
-
-    return indices
+    idx = np.atleast_1d(idx)
+    n = np.atleast_1d(n)
+    return np.array(source_galaxy_index_selection_kernel(idx, n))
 
