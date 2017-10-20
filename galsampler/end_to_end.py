757d2be4498bcf481e446c3f891a36720c7fb077
Andrew Hearin <andrew.hearin@yale.edu>
2017-10-20 17:39:58 -0500

    Fixed bugs in source_galaxy_selection_indices and added dictionary key correspondence feature. Notebook end_to_end_demo.ipynb demonstrates that the recovered SMF is incorrect

--- galsampler/end_to_end.py
+++ galsampler/end_to_end.py
@@ -8,28 +8,32 @@ from .source_galaxy_selection import source_galaxy_index_selection
 
 
 def source_galaxy_selection_indices(source_galaxies, source_halos, target_halos,
-            nhalo_min, *bins):
+            nhalo_min, *bins, **kwargs):
     """
     Examples
     --------
     source_galaxies : Numpy structured array
         Ndarray of shape (num_source_gals, ) storing the source galaxies that
         occupy the source halos.
-        Column names must include ``halo_id`` and ``host_halo_id``.
+        Column names must include ``halo_id`` and ``host_halo_id``,
+        or otherwise be defined via the optional source_galaxies_colnames keyword.
 
     source_halos : Numpy structured array
         Ndarray of shape (num_source_halos, ) storing the catalog of host halos
         that are occupied by the source galaxies.
-        Column names must include ``halo_id``, and ``bin_number``.
+        Column names must include ``halo_id``, and ``bin_number``,
+        or otherwise be defined via the optional source_halos_colnames keyword.
+
         The ``bin_number`` column can be computed using the
-        `galsampler.host_halo_binning.halo_bin_indices` function.
+        `galsampler.halo_bin_indices` function.
 
     target_halos : Numpy structured array
         Ndarray of shape (num_target_halos, ) storing the catalog of host halos
         that will become populated by a Monte Carlo sampling of the source galaxies.
         Column names must include ``bin_number``.
+
         The ``bin_number`` column can be computed using the
-        `galsampler.host_halo_binning.halo_bin_indices` function.
+        `galsampler.halo_bin_indices` function.
 
     nhalo_min : int
         Minimum permissible number of halos in source catalog for a cell to be
@@ -44,30 +48,44 @@ def source_galaxy_selection_indices(source_galaxies, source_halos, target_halos,
         Numpy integer array of shape (num_target_gals, ) storing the indices
         of the selected galaxies
     """
+    source_galaxies_colnames = dict(halo_id='halo_id', host_halo_id='host_halo_id')
+    source_galaxies_colnames.update(kwargs.get('source_galaxies_colnames', {}))
+    _check_colname_correspondence_dictionary(source_galaxies_colnames, source_galaxies, "source_galaxies")
+
+    source_halos_colnames = kwargs.get('source_halos_colnames',
+            dict(halo_id='halo_id', bin_number='bin_number'))
+
+    target_halos_colnames = kwargs.get('target_halos_colnames',
+            dict(bin_number='bin_number'))
+
     source_halos['richness'] = compute_richness(
-                source_halos['halo_id'], source_galaxies['host_halo_id'])
+                source_halos[source_halos_colnames['halo_id']],
+                source_galaxies[source_galaxies_colnames['host_halo_id']])
 
     #  Broadcast the ``bin_number`` and ``richness`` columns
     #  from the source halos to the source galaxies
-    idxA, idxB = crossmatch(source_galaxies['host_halo_id'], source_halos['halo_id'])
+    idxA, idxB = crossmatch(source_galaxies[source_galaxies_colnames['host_halo_id']],
+            source_halos[source_halos_colnames['halo_id']])
     source_galaxies['bin_number'] = 0
     source_galaxies['richness'] = 0
-    source_galaxies['bin_number'][idxA] = source_halos['bin_number'][idxB]
+    source_galaxies['bin_number'][idxA] = source_halos[source_halos_colnames['bin_number']][idxB]
     source_galaxies['richness'][idxA] = source_halos['richness'][idxB]
 
     #  Sort the source galaxies so that members of a common halo are grouped together
-    source_galaxies.sort(['bin_number', 'host_halo_id'])
+    source_galaxies.sort(source_galaxies_colnames['host_halo_id'])
 
     #  For every target halo, calculate the index of a source halo whose resident
     #  galaxies will populate the target halo.
     source_halo_selection_indices = source_halo_index_selection(
-            source_halos['bin_number'], target_halos['bin_number'], nhalo_min, *bins)
+            source_halos[source_halos_colnames['bin_number']],
+            target_halos[target_halos_colnames['bin_number']], nhalo_min, *bins)
 
     #  For each selected source halo, determine the index of the first
     #  appearance of a source galaxy that resides in that halo
     #  The algorithm below is predicated upon the source galaxies being sorted by ``host_halo_id``
-    uval, indx_uval = np.unique(source_galaxies['host_halo_id'], return_index=True)
-    selected_halo_ids = source_halos['halo_id'][source_halo_selection_indices]
+    uval, indx_uval = np.unique(
+        source_galaxies[source_galaxies_colnames['host_halo_id']], return_index=True)
+    selected_halo_ids = source_halos[source_halos_colnames['halo_id']][source_halo_selection_indices]
     idxA, idxB = crossmatch(selected_halo_ids, uval)
     representative_galaxy_selection_indices = indx_uval[idxB]
 
@@ -75,3 +93,18 @@ def source_galaxy_selection_indices(source_galaxies, source_halos, target_halos,
     #  for each selected source halo
     return source_galaxy_index_selection(representative_galaxy_selection_indices,
                         source_galaxies['richness'][representative_galaxy_selection_indices])
+
+
+def _check_colname_correspondence_dictionary(d, catalog, catalog_varname):
+    """
+    """
+    keyword_name = catalog_varname + "_colnames"
+    for key, value in d.items():
+        try:
+            assert value in list(catalog.keys())
+        except AssertionError:
+            msg = ("{0} does not contain a ``{1}`` column name\n"
+                "Either rename some column in {0} or use the {2} keyword")
+            raise KeyError(msg.format(catalog_varname, value, keyword_name))
+
+
