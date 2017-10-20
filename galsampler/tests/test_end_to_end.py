fc9eab895bf6e247c1cde642b0902ccd5868ef16
Andrew Hearin <andrew.hearin@yale.edu>
2017-10-20 18:30:09 -0500

    Added new unit-testing module

---
+++ galsampler/tests/test_end_to_end.py
@@ -0,0 +1,27 @@
+"""
+"""
+import pytest
+import numpy as np
+
+
+@pytest.mark.xfail
+def test_bijective_case():
+    """
+    """
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_constant_scaleup_case():
+    """
+    """
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_empty_halos_case():
+    """
+    """
+    raise NotImplementedError
+
+
