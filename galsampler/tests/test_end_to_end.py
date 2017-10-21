7e0f2d93b0b64f18168c9b5f663e2b7292d987b8
Andrew Hearin <andrew.hearin@yale.edu>
2017-10-20 19:11:06 -0500

    Added test2_bijective_case. Fails.

--- galsampler/tests/test_end_to_end.py
+++ galsampler/tests/test_end_to_end.py
@@ -50,9 +50,54 @@ def test1_bijective_case():
     assert np.all(indices == np.arange(len(indices)))
 
 
-@pytest.mark.xfail
 def test2_bijective_case():
     """
+    Setup:
+
+    * Each source halo belongs to a unique bin.
+    * There exists 5 target halos for every source halo.
+    * Each source halo is populated with a single galaxy.
+
+    Verify:
+
+    * The target galaxy catalog is a 5x repetition of the source galaxy catalog
+    """
+    num_source_halos = 10
+    num_galaxies = num_source_halos
+    num_target_halos = num_source_halos*5
+    nhalo_min = 1
+
+    source_halo_dt_list = [(str('halo_id'), str('i8')), (str('bin_number'), str('i4'))]
+    source_halos_dtype = np.dtype(source_halo_dt_list)
+    source_halos = np.zeros(num_source_halos, dtype=source_halos_dtype)
+    source_halos['halo_id'] = np.arange(num_source_halos).astype(int)
+    source_halos['bin_number'] = np.arange(num_source_halos).astype(int)
+
+    source_galaxy_dt_list = [(str('halo_id'), str('i8')), (str('host_halo_id'), str('i4'))]
+    source_galaxies_dtype = np.dtype(source_galaxy_dt_list)
+    source_galaxies = np.zeros(num_galaxies, dtype=source_galaxies_dtype)
+    source_galaxies['halo_id'] = np.arange(num_galaxies).astype(int)
+    source_galaxies['host_halo_id'] = np.arange(num_galaxies).astype(int)
+
+    target_halo_dt_list = [(str('bin_number'), str('i4'))]
+    target_halos_dtype = np.dtype(target_halo_dt_list)
+    target_halos = np.zeros(num_target_halos, dtype=target_halos_dtype)
+    target_halos['bin_number'] = np.arange(num_target_halos).astype(int)
+
+    fake_bins = np.arange(num_source_halos)
+    indices = source_galaxy_selection_indices(source_galaxies, source_halos, target_halos,
+            nhalo_min, fake_bins)
+    selected_galaxies = source_galaxies[indices]
+    assert len(selected_galaxies) == num_target_halos
+    assert np.all(selected_galaxies == np.repeat(source_galaxies, 5))
+
+
+@pytest.mark.xfail
+def test3_bijective_case():
+    """
+    Identical setup to test1_bijective_case,
+    but the initialization uses the host_halo_binning_module
+    for integration testing purposes.
     """
     log_mhost_min, log_mhost_max, dlog_mhost = 9.5, 15.5, 0.5
     log_mhost_bins = np.arange(log_mhost_min, log_mhost_max+dlog_mhost, dlog_mhost)
