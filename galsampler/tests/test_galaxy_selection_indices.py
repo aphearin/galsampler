8d8ca21aea6fe954fba80a6826905e8f5f3cd495
Andrew Hearin <andrew.hearin@yale.edu>
2017-10-21 13:47:49 -0500

    Added expected test failure to still-not-implemented unit-testing module

--- galsampler/tests/test_galaxy_selection_indices.py
+++ galsampler/tests/test_galaxy_selection_indices.py
@@ -1,13 +1,15 @@
 """
 """
+import pytest
 import numpy as np
 
 
 from ..source_galaxy_selection import source_galaxy_index_selection
 
 
+@pytest.mark.xfail
 def test1():
     n = [1, 3, 2]
     galaxies = np.arange(np.sum(n))
     idx = [0, 1, 1]
-    raise NotImplementedError("source_galaxy_index_selection function still untested")
+    raise NotImplementedError("source_galaxy_index_selection function still has no unit-testing")
