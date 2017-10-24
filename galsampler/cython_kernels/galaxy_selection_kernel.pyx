41232219cb0b073cb3bb21ae6a4e079e6df991b9
Andrew Hearin <andrew.hearin@yale.edu>
2017-10-24 14:41:36 -0500

    Removed obsolete module

--- galsampler/cython_kernels/galaxy_selection_kernel.pyx
+++ galsampler/cython_kernels/galaxy_selection_kernel.pyx
@@ -6,34 +6,7 @@ cimport cython
 import numpy as np
 
 
-__all__ = ('source_galaxy_index_selection_kernel', 'galaxy_selection_kernel')
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-def source_galaxy_index_selection_kernel(long[:] idx, long[:] n):
-    """
-    """
-    cdef int num_source_halos = idx.shape[0]
-
-    cdef int i, num_target_gals
-    for i in range(num_source_halos):
-        num_target_gals += n[i]
-
-    cdef long[:] indices = np.zeros(num_target_gals).astype(long)
-
-    cdef long cur = 0
-    cdef long ifirst, j, nselect
-
-    for i in range(num_source_halos):
-        ifirst = idx[i]
-        nselect = n[i]
-        for j in range(nselect):
-            indices[cur] = ifirst + j
-            cur += 1
-
-    return indices
+__all__ = ('galaxy_selection_kernel', )
 
 
 @cython.boundscheck(False)
--- galsampler/tests/test_galaxy_selection_indices.py
+++ galsampler/tests/test_galaxy_selection_indices.py
@@ -4,12 +4,12 @@ import pytest
 import numpy as np
 
 
-from ..source_galaxy_selection import source_galaxy_index_selection
+# from ..source_galaxy_selection import source_galaxy_index_selection
 
 
-@pytest.mark.xfail
-def test1():
-    n = [1, 3, 2]
-    galaxies = np.arange(np.sum(n))
-    idx = [0, 1, 1]
-    raise NotImplementedError("source_galaxy_index_selection function still has no unit-testing")
+# @pytest.mark.xfail
+# def test1():
+#     n = [1, 3, 2]
+#     galaxies = np.arange(np.sum(n))
+#     idx = [0, 1, 1]
+#     raise NotImplementedError("source_galaxy_index_selection function still has no unit-testing")
