c617b48c4e9d1c50d79fddbe679679618935847a
Andrew Hearin <andrew.hearin@yale.edu>
2018-02-20 11:22:26 -0600

    Added matched_value_selection_indices to be top-level importable

--- galsampler/__init__.py
+++ galsampler/__init__.py
@@ -11,3 +11,4 @@ if not _ASTROPY_SETUP_:
     from .host_halo_binning import halo_bin_indices, matching_bin_dictionary
     from .source_halo_selection import source_halo_index_selection
     from .source_galaxy_selection import source_galaxy_selection_indices
+    from .matched_halo_selection_1d import matched_value_selection_indices
